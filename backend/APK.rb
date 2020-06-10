30bfbc21cb5654d1d8203435f0699d5bb3d330d8
MysticPing <hajjvictor@live.se>
2020-06-10 21:56:13 +0200

    Prettier logging when running

--- backend/APK.rb
+++ backend/APK.rb
@@ -2,9 +2,15 @@ require_relative "downloader"
 require_relative "extractor"
 require_relative "importer"
 
-dl = Downloader.new
+puts ""
+puts Time.now
+
+dl = Downloader.new(max_attempts: 7, backoff: 2.5)
 ex = Extractor.new
 im = Importer.new
 doc = dl.download("https://www.systembolaget.se/api/assortment/products/xml")
 ext = ex.extract(doc) 
-im.import(ext)
+im.import(ext, "apk.db")
+
+puts ""
+
