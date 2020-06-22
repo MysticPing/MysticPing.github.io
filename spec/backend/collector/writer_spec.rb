3940dba534dc6e9c3adf049e13d4f4affc4728b6
MysticPing <hajjvictor@live.se>
2020-06-22 21:56:12 +0200

    Added writer spec

--- spec/backend/collector/writer_spec.rb
+++ spec/backend/collector/writer_spec.rb
@@ -1,28 +1,34 @@
 require_relative "../../../backend/collector/writer"
 
-describe Importer do
-    describe "importer" do
-        before (:all) do
-            @wr = Importer.new
-            @data = 
-            [
-                {
-                    id: 2,
-                    apk: 18.667, 
-                    name: '4 5',
-                    price: 7.5,
-                    volume: 700.0,
-                    style: '9',
-                    type: '8',
-                    abv: 0.2,
-                    availability: '10'
-                }
-            ]
-        end
-        context "given data" do
-            it "write to sql db" do
-                result = @im.import(@apk_list)
-                expect(result).to eql(@expected_result)
+describe Writer do
+    describe "writer" do
+        context 'given data' do
+            let :writer do
+                Writer.new(':memory:')
+            end
+
+            let :data do
+                [
+                    {
+                        id: 2,
+                        apk: 18.667, 
+                        name: '4 5',
+                        price: 7.5,
+                        volume: 700.0,
+                        style: '9',
+                        type: '8',
+                        abv: 0.2,
+                        availability: '10'
+                    }
+                ]
+            end
+            
+            let :expected_result do
+                [[2, 18.667, '4 5', 7.5, 700.0, '8', '9', 0.2, '10']]
+            end
+
+            it 'write to db' do
+                expect(writer.write(data).execute('SELECT * FROM apk;')).to eql(expected_result)
             end
         end
     end
